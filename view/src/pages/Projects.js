3b97d4d6b359c22ab7a32cdd0944b8a92e74b230
ventusboy <42723217+ventusboy@users.noreply.github.com>
2021-11-28 16:11:36 -0500

    comments removed

--- view/src/pages/Projects.js
+++ view/src/pages/Projects.js
@@ -14,20 +14,16 @@ class Projects extends React.Component {
         // eslint-disable-next-line
         axios.get('http://localhost:5000/portfolio-610be/us-central1/api/getProjects'||'/api/getProjects').
             then(({ data }) => {
-                console.log(data)
                 let projectsArray = []
                 for (const key in data) {
                     projectsArray.push(data[key])
                 }
-                console.log(projectsArray)
                 let projects = projectsArray.map(element => {
                     return <Project key = {element.name} data = {element} />
                 });
                 this.setState({
                     projects
                 })
-                console.log(projects)
-                // return projectsArray
             }).catch(() => {
                 console.log('yeet')
             })
