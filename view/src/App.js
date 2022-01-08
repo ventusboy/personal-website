ca7b00a98fc0f812603456d56bdb123b36e81ba3
Mikal Young <mikal@floatleft.tv>
2022-01-08 18:21:35 -0500

    hamburger added to mobile

--- view/src/App.js
+++ view/src/App.js
@@ -11,10 +11,20 @@ function App() {
       <header className="navbar">
         <h1 className="logo"><a href="/index">Mikal Young</a></h1>
         <ul id="links">
-            <li><button id="link0" >Home</button></li>
-            <li><button id="link1" >About Me</button></li>
-            <li><button id="link2" >Projects</button></li>
+          <li><button id="link0" >Home</button></li>
+          <li><button id="link1" >About Me</button></li>
+          <li><button id="link2" >Projects</button></li>
         </ul>
+        <div className='toggle'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div id='mobile-links'>
+          <li><button id="link0" >Home</button></li>
+          <li><button id="link1" >About Me</button></li>
+          <li><button id="link2" >Projects</button></li>
+        </div>
       </header>
       <Home />
       <About />
