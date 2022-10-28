d77e7251da93c37d37dc3fc21e2a2f3ca940eaf1
Mikal Young <mikal@floatleft.tv>
2022-10-28 12:54:23 -0400

    init commit

--- view/src/App.js
+++ view/src/App.js
@@ -3,17 +3,18 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import React from 'react';
-import './assets/css/main1.scss'
+import BackGround from './components/BackGround'
+import './assets/css/main.scss'
 import { useState } from 'react';
 
 function App() {
-  
-  const [ toggleMenu, setToggleMenu ] = useState(false)
-  
-  function animate () {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  function animate() {
     return toggleMenu ? 'slide-left' : 'slide-right';
   }
-  function goTo (page) {
+  function goTo(page) {
     // window.location.href=`#page${page}`
     var pageAnchor = document.getElementById(`page${page}`)
     pageAnchor.scrollIntoView()
@@ -22,26 +23,30 @@ function App() {
 
   return (
     <div className="App">
-      <header className="navbar">
+      <BackGround />
+      <div className="navbar">
         <h1 className="logo"><a href="/index">Mikal Young</a></h1>
         <ul id="links">
           <li><button onClick={() => { goTo(1) }}>Home</button></li>
           <li><button onClick={() => { goTo(2) }}>About Me</button></li>
           <li><button onClick={() => { goTo(3) }} >Projects</button></li>
         </ul>
-        <div className={'toggle ' + (toggleMenu ? 'toggle-active' : '')} onClick={() => {setToggleMenu(!toggleMenu)}}>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div id='mobile-links' className={animate()}>
-          <ul>
-            <li><button onClick={() => { goTo(1) }}>Home</button></li>
-            <li><button onClick={() => { goTo(2) }}>About Me</button></li>
-            <li><button onClick={() => { goTo(3) }}>Projects</button></li>
-          </ul>
-        </div>
-      </header>
+        {/*<div id='mobile-toggle'>
+          <div className={'toggle ' + (toggleMenu ? 'toggle-active' : '')} onClick={() => { setToggleMenu(!toggleMenu) }}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div id='mobile-links' className={animate()}>
+            <ul>
+              <li><button onClick={() => { goTo(1) }}>Home</button></li>
+              <li><button onClick={() => { goTo(2) }}>About Me</button></li>
+              <li><button onClick={() => { goTo(3) }}>Projects</button></li>
+            </ul>
+          </div>
+        </div>*/}
+
+      </div>
       <div id='pages'>
         <Home />
         <About />
